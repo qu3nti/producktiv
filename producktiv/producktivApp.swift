ea33a6fca7df50c9898ec382e5016b00dd11f1f9
Quentin <qpompliano@gmail.com>
2023-01-09 09:17:03 -0500

    Initial Commit

---
+++ producktiv/producktivApp.swift
@@ -0,0 +1,17 @@
+//
+//  producktivApp.swift
+//  producktiv
+//
+//  Created by Quentin Pompliano on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct producktivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
